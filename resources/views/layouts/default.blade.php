17fc0395647953cf73befb6dab0b4a0d453351ef
derek-5 <wjn123562077@gmail.com>
2021-04-08 11:18:14 -0400

    initial style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Weibo App')</title>
+    <title>@yield('title', 'Weibo App') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
-@section('title', 'Home Page')
+
 @section('content')
-  <h1>Home Page</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+       <a href="https://learnku.com/courses/laravel-essential-training">Laravel Guide</a>
+    </p>
+    <p>
+      Start From Here
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+    </p>
+  </div>
 @stop
