5c55f14f52fc5c3dea7a456ee5ef606e809ead63
wjn123562077@gmail.com <wjn123562077@gmail.com>
2021-04-14 20:56:05 +0000

    stats info

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -127,20 +127,4 @@ class UsersController extends Controller
         session()->flash('success', 'Congratulations! Activation success!');
         return redirect()->route('users.show', [$user]);
     }
-
-    public function follow($user_ids)
-    {
-        if( !is_array($user_ids)){
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->sync($user_ids, false);
-    }
-
-    public function unfollow($user_ids)
-    {
-        if( !is_array($user_ids)){
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->detach($user_ids);
-    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersTableSeeder::class,
-            StatusesTableSeeder::class
+            StatusesTableSeeder::class,
+            FollowersTableSeeder::class
         ]);
 
         Model::reguard();
